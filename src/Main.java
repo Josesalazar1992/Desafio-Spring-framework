b6e5cd59cdda872d539a2445290c5e45bb961dca
jopasari@gmail.com <jopasari@gmail.com>
2024-07-05 19:17:27 -0600

    Start project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    // 1. Covertir el Json a un texto legible.
+    // 2. Obtener un top 10
+    // 3. Crear un metodo para buscar
+    // 4. Mostrar estadistica
+
+    public static void main(String[] args) {
+
+        }
+    }
